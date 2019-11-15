a0349308fac07e9f834023017c92aacd15383e45
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-11-15 15:24:48 -0600

    fix code

--- src/LinkGenerator.test.js
+++ src/LinkGenerator.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LinkGenerator from './LinkGenerator'
+import {render, fireEvent} from 'react-testing-library'
 
 it('successful render', () => {
     const div = document.createElement('div');
-    const {getByText, getByTestId, container} = render(<LinkGenerator link="dummy.com" />, div);
+    const {getByTestId} = render(<LinkGenerator link="dummy.com" />, div);
     const elem = getByTestId('link-holder');
     expect(elem.innerHTML).toBe('dummy.com');
     ReactDOM.unmountComponentAtNode(div);
@@ -12,7 +13,7 @@ it('successful render', () => {
 
 it('link clickable', () => {
     const div = document.createElement('div');
-    const {getByText, getByTestId, container} = render(<LinkGenerator link="dummy.com" />, div);
+    const {getByText, getByTestId} = render(<LinkGenerator link="dummy.com" />, div);
     fireEvent.click(getByText('Copy'));
     const elem = getByTestId('copy-button');
     expect(elem.innerHTML).toBe("Copied!");
